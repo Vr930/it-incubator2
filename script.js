125617bfd755baf49565a2ea33d886c219b728be
Vladislav <you@example.comvr@mail.ru>
2022-12-30 14:13:02 +0300

    fix js

--- script.js
+++ script.js
@@ -11,3 +11,33 @@ function testLessOrEqual(val) {
   }
 
   testLessOrEqual(10);
+
+  function Dog (name) {
+    this.myName = name;
+    this.voice = function () {
+        console.log('hay, my name is ' + this.myName)
+    }
+}
+const bulDog = new Dog('buldog');
+bulDog.voice()
+/////////////////////////////////////////////////////////
+
+const chainMaker = {
+    current : [],
+    getLength : function() {
+       return this.current.length;
+    },
+    addLink : function(value) {
+        if(value === undefined) {
+            this.current.push(`( ${''} )`);
+        }
+        else if(this.current.push(`( ${value} )`)) {
+        }
+        return this
+    },
+    finishChain : function() {
+      return this.current.join('~~')
+    }
+}
+console.log(chainMaker.addLink(1).addLink().addLink(3).finishChain());
+
